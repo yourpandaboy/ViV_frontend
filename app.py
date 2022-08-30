37866140ba04e2c268370da1d380aaec058c32bc
Norty Nakagawa <norty.nakagawa@gmail.com>
2022-08-30 11:02:51 +0900

    editing subheaders

--- app.py
+++ app.py
@@ -51,9 +51,10 @@ with row1_2:
     row1_2.title("Hi! I'm ViV, your personal matchmaker!")
     row1_2.subheader(
     """
-        Not only do I match you with someone based on the preferences you set,
-        I also consider the information you write in your bio! This is called my **“Vibes-Tingle”** which helps me to find the most suitable match for you.
+
+        Not only do I match you with someone based on the preferences you set, I also consider the information you write in your bio! This is called my **“Vibes-Tingle”** which helps me to find the most suitable match for you.
         So go ahead and get started, and remember: *the more information you fill out in your bio, the stronger my Vibes-Tingle will set off in finding you a match!*
+
     """)
     row1_2.write('**Created by Norty Nakagawa! Connect with me on [github](https://github.com/yourpandaboy) and [linkedin](https://www.linkedin.com/in/norutado-nakagawa/)**')
 
